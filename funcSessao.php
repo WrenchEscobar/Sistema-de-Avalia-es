9ef360fc9b5812125963e8736e288ece7233104e
Juan <ju_pn2010@hotmail.com>
2019-05-08 17:01:44 -0300

    Função

--- funcSessao.php
+++ funcSessao.php
@@ -1,5 +1,16 @@
 <?php
 
+    function logar($usuario, $senha, $profissao){
+        
+        // Verifica no MySQL a existência do usuário e senha para realizar o login caso seja
+        // A variável profissão deverá ser recebida ou como "Alunos" ou como "Professor"
+            $pesquisa = "select * from ".$profissao;
+            $resultadoPesquisa = mysqli_fetch_array($pesquisa);
+        ////////////////////////////////////////////////////////////////////////////////////
+
+        // Caso o usuário exista o login é realizado
+        foreach()
+    }
 
 
 ?>
